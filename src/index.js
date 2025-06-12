669f4f4a822f9c0968353e68afa633d709548f58
kushal-24 <kushalphadnis2015@gmail.com>
2025-06-12 13:53:08 +0530

    app created

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ dotenv.config({
 import connectDB from "./db/index.js"
 
 connectDB()
-// .then(()=>{
-//     app.listen(process.env.PORT||8000, ()=>{
-//         console.log(`Server is running on some port`)
-//     })
-// })
-// .catch((err)=>{
-//     console.log("MongoDB connection failed !!! :", err)
-// })
+.then(()=>{
+    
+    app.listen(process.env.PORT||8000, ()=>{
+        console.log(`Server is running on some port`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!! :", err)
+})
