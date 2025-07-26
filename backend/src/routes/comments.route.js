f2c44bfe943709d9f17a38b275dcc0dbf0cba654
kushal-24 <kushalphadnis2015@gmail.com>
2025-07-26 12:03:59 +0530

    role verification done

--- backend/src/routes/comments.route.js
+++ backend/src/routes/comments.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.route("/comment").post();
-router.route("/comment/:eventId").get();
+//router.route("/comment").post();
+//router.route("/comment/:eventId").get();
 
 export default router;
